2884ee77c6fa4098df0a366f06b921a2d8af7b54
Shashikant <shashikant.1997k@gmail.com>
2020-09-05 13:06:34 +0530

    Updates

---
+++ precache-manifest.089ddbf3b5f404062a87842fd59dba25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38475e82f7256eabc38a553089ada5bc",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "690d2eeb594dffb8454e",
+    "url": "/whatsapp-clone/static/css/main.f1730986.chunk.css"
+  },
+  {
+    "revision": "b20e9b1887294f4b3e1c",
+    "url": "/whatsapp-clone/static/js/2.740ce6f5.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whatsapp-clone/static/js/2.740ce6f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690d2eeb594dffb8454e",
+    "url": "/whatsapp-clone/static/js/main.27240b99.chunk.js"
+  },
+  {
+    "revision": "8d0a8cdab6927a1f4f08",
+    "url": "/whatsapp-clone/static/js/runtime-main.a04c35f0.js"
+  }
+]);
